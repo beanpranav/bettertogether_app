24da5697ef3345cae6a092520b6faee15e887cd3
pragya <saxena.pragya8@gmail.com>
2015-05-25 22:03:29 -0400

    fixing the content a bit
    fixing the content a bit

--- src/com/iwl/bettertogforever/SecretMessageTabFragment.java
+++ src/com/iwl/bettertogforever/SecretMessageTabFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class SecretMessageTabFragment extends Fragment{
 
@@ -17,8 +16,6 @@ public class SecretMessageTabFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.secret_msg_tab_fragment, container, false);
-        TextView tv = (TextView) v.findViewById(R.id.secretMsg_tab1);
-        tv.setText(this.getTag() + " Content");
         return v;
     }
 }
